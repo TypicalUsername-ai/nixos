bb0d67ce16e5bedd53fab01904189975d1b649b7
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-10-16 14:30:19 +0000

    nvim: retry with cloning config and keeping it separate

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,5 +1,12 @@
 { config, pkgs, ...}:
 {
+
+# Fetch and clone the Neovim config repository
+	home.file.".config/nvim".source = builtins.fetchGit {
+	url = "https://github.com/TypicalUsername-ai/neovim-setup.git";  # Replace with your repo URL
+	ref = "nixos";  # Replace with your branch, tag, or commit
+  };
+
     programs.neovim = {
       enable = true;
       defaultEditor = true;
@@ -20,9 +27,29 @@
         trouble-nvim
         which-key-nvim
       ];
-	extraLuaConfig = ''
-	vim.g.mapleader = " "
-	require("keymaps")
-	'';
+      extraLuaConfig = ''
+      	require("config.keymaps")
+	reuire("config.options")
+	vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
+    	require("lazy").setup({
+      		spec = {
+        	-- Import plugins from lua/plugins
+        	{ import = "plugins" },
+      },
+      performance = {
+        reset_packpath = false,
+        rtp = {
+            reset = false,
+          }
+        },
+      dev = {
+        path = "${pkgs.vimUtils.packDir config.home-manager.users.USERNAME.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
+      },
+      install = {
+        -- Safeguard in case we forget to install a plugin with Nix
+        missing = false,
+      },
+    })
+      '';
     };
   }
