52542a0eae83e7a0e98cda4f0adff830d79d3e27
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-11-13 16:28:59 +0000

    devtools: add cargo-tarpaulin

--- modules/devtools.nix
+++ modules/devtools.nix
@@ -11,7 +11,9 @@
   environment.systemPackages = with pkgs; [
     neovim
     docker
+    ## Rust-specific consider moving or abstracting to option TODO
     bacon
+    cargo-tarpaulin
   ];
 
   environment.variables.EDITOR = "nvim";
