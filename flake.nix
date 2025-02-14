88ecf275b921914fcf8c6d816815b19b7c712068
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-02-14 21:42:02 +0000

    add rust overlay

--- flake.nix
+++ flake.nix
@@ -4,7 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/nixos-wsl";
-
+    # For automatically installing the rust toolchain
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,6 +20,7 @@
       self,
       nixpkgs,
       home-manager,
+      rust-overlay,
       ...
     }@inputs:
     {
@@ -25,6 +30,10 @@
         };
         modules = [
           ./hosts/wsl/configuration.nix
+          #        ({pkgs, ... }): {
+          #                   nixpkgs.overlays = [ rust-overlay.overlays.default ];
+          #                   environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
+          #             }
           home-manager.nixosModules.home-manager
           {
             home-manager.users.matt = import ./home.nix;
