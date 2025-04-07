7b813595434fd20a7f2594a6a171a4d3928ad21e
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-04-07 12:29:33 +0000

    modules: add python

---
+++ modules/python.nix
@@ -0,0 +1,30 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+
+}:
+let
+  pythonEnv = pkgs.python313.withPackages (ps: [
+    ps.numpy
+    ps.polars
+    ps.jupyter
+    ps.matplotlib
+    ps.pytorch
+    ps.requests
+  ]);
+in
+{
+
+  environment.systemPackages = with pkgs; [
+    pythonEnv
+  ];
+}
