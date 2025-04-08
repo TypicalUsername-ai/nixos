9372e377b8b35fcbff827d163a9e2658d90fc0d0
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-04-09 00:06:40 +0200

    homelab: starter wireguard config

--- hosts/rpi-homelab/configuration.nix
+++ hosts/rpi-homelab/configuration.nix
@@ -25,20 +25,70 @@
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
 
-  # add wireguard kernel package
-  boot.extraModulePackages = [ config.boot.kernelPackages.wireguard ];
-
   networking.hostName = "rpi-homelab"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+
+  ## wireguard config from [nixos docs]()
+  # enable NAT
+  networking.nat.enable = true;
+  networking.nat.externalInterface = "end0";
+  networking.nat.internalInterfaces = [ "wg0" ];
+  networking.firewall = {
+    allowedUDPPorts = [ 51820 ];
+  };
+
+  networking.wireguard.enable = true;
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the server's end of the tunnel interface.
+      ips = [ "10.100.0.1/24" ];
+
+      # The port that WireGuard listens to. Must be accessible by the client.
+      listenPort = 51820;
+
+      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+      # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      # This undoes the above command
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = "~/wireguard/private_key";
+
+      peers = [
+        # List of allowed peers.
+        {
+          # SM 55a PHONE
+          # Public key of the peer (not a file path).
+          publicKey = "eQNsO1CDUkVJuobv68Ep270KoSpw1hXgVAWoFsQ2D3Y=";
+          # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
+          allowedIPs = [ "10.100.0.2/32" ];
+        }
+
+      ];
+    };
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
 
   environment.systemPackages = with pkgs; [
     mold
+    # add wireguard packages
     # wireguard-go #!! broken
+    # pkgs.linuxKernel.packages.linux_5_4.wireguard
     wireguard-tools
   ];
 
