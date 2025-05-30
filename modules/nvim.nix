9a7945437c28179ea2db5f4e329f56dfcd265454
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-05-30 19:44:07 +0000

    nvim: bumpt to 1.2.7 and try fix cache issues

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,11 +1,16 @@
-{ pkgs, home-manager, ... }:
+{
+  config,
+  pkgs,
+  home-manager,
+  ...
+}:
 {
 
   # Fetch and clone the Neovim config repository
   home.file.".config/nvim".source = pkgs.fetchFromGitHub {
     owner = "TypicalUsername-ai";
     repo = "neovim-setup";
-    rev = "v1.2.6";
+    rev = "v1.2.7";
     sha256 = "sha256-pxskfzBpVSxrRDPjwu9Q3PSxpHN0imwAd3F6n5XV3Gk=";
   };
 
@@ -16,6 +21,7 @@
     vimAlias = true;
     vimdiffAlias = true;
     plugins = with pkgs.vimPlugins; [
+      alpha-nvim
       conform-nvim
       gitsigns-nvim
       nvim-lspconfig
@@ -31,25 +37,27 @@
       lazy-nvim
     ];
     extraLuaConfig = ''
-        require("config.keymaps")
-      	require("config.options")
-      	vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
-          	require("lazy").setup({
-            		spec = {
-              	-- Import plugins from lua/plugins
-              	{ import = "plugins" },
-            },
-            performance = {
-              reset_packpath = false,
-              rtp = {
-                  reset = false,
-                }
-              },
-            install = {
-              -- Safeguard in case we forget to install a plugin with Nix
-              missing = false,
-            },
-          })
+      vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
+      require("lazy").setup({
+        spec = {
+          -- Import plugins from lua/plugins
+          { import = "plugins" },
+        },
+        performance = {
+          reset_packpath = false,
+          rtp = {
+              reset = false,
+            }
+          },
+        dev = {
+          path = "${pkgs.vimUtils.packDir config.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
+          patterns = {""}, -- Specify that all of our plugins will use the dev dir. Empty string is a wildcard!
+        },
+        install = {
+          -- Safeguard in case we forget to install a plugin with Nix
+          missing = false,
+        },
+      })
     '';
   };
 }
