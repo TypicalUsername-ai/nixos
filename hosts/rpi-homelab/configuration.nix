1716a9c3999c0c830cfd24d8804c881dd317e921
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-08-12 00:18:42 +0200

    cleanup: deprecate dynamic dns service and remove unnecessary docs

--- hosts/rpi-homelab/configuration.nix
+++ hosts/rpi-homelab/configuration.nix
@@ -49,15 +49,6 @@
 
   
 # Dynamic DNS systemd service
-  systemd.services = {
-      dynamic-dns-updater = {
-          path = [
-              pkgs.curl
-          ];
-          script = "/home/matt/noip/update.sh";
-          startAt = "hourly";
-      };
-  };
 
 # virtualisation socket settings
   virtualisation.podman.networkSocket = {
@@ -105,21 +96,6 @@
       };
   services.fail2ban.enable = true;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
   # This value being lower than the current NixOS release does NOT mean your system is
   # out of date, out of support, or vulnerable.
   #
